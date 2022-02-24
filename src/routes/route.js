7da7281c678146e062de00cac3eba4119212d655
Debjani97 <81793397+Debjani97@users.noreply.github.com>
2022-02-24 21:57:27 +0530

    Update route.js

--- src/routes/route.js
+++ src/routes/route.js
@@ -6,6 +6,60 @@ router.get('/students/:name', function(req, res) {
     console.log(studentName)
     res.send(studentName)
 })
+let players = [];
+
+router.post('/players', function(req, res) {
+
+    let player = req.body;
+    let playerName = player.name
+    for(let i = 0; i < players.length; i++){
+        if(players[i].name == playerName){
+            res.send('player already exist')
+        }
+    }
+    players.push(player);
+    console.log('here is the playter array',players);
+    res.send(players);
+});
+
+router.post('/players/:playerName/bookings/:bookingId', function(req, res){
+    let name = req.params.playerName
+    let isPlayerPresent = false
+
+    for(let i = 0; i < players.length; i++) {
+        if(players[i].name == name) {
+        isPlayerPresent = true
+        }
+    }
+
+    if(! isPlayerPresent){
+        return res.send('player not present')
+    }
+
+    let booking = req.body
+    let bookindId = req.params.bookingId
+    for(let i = 0; i < players.length; i++){
+        if(players[i].name == name) {
+            let isBookingPresent = false
+            for(let j = 0; j < players[i].bookings.length; j++){
+                if(players[i].bookings[j]){
+                    players[i].bookings.push(booking)
+                }
+            }
+            
+        }
+    }
+
+    res.send(players)
+
+})
+    
+
+router.post('/student/:name', function(req, res) {
+    let studentName = req.params.name
+    console.log(studentName)
+    res.send(studentName)
+});
 
 router.get("/random" , function(req, res) {
     res.send("hi there")
