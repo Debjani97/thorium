00e53e5c7b41623b73e847fdcf45dfe7ba6e5d24
Debjani97 <debjanicob97@gmail.com>
2022-03-01 02:23:42 +0530

    holo na

--- src/models/bookModel.js
+++ src/models/bookModel.js
@@ -1,20 +1,25 @@
-const mongoose = require('mongoose');
-
-const bookSchema = new mongoose.Schema( {
-    bookName: String, 
-    authorName: String, 
-    tags: [String],
-    
-    isPublished: Boolean,
-    prices: {
-        indianPrice: String,
-        europePrice: String,
+const bookSchema = new mongoose.Schema({
+    bookName : {
+        type : String,
+        required : true,
+        unique : true
     },
-    sales: {type: Number, default: 10}
-}, { timestamps: true });
-
-
-module.exports = mongoose.model('Book', bookSchema) //users
+    authorName : {
+        type : String,
+        required : true
+    },
+    price :{
+        indianPrice : String,
+        EuropeanPrice : String
+    },
+    year : {
+        type : Number,
+        default : 2021
+    },
+    totalPages : Number,
+    tags : [String],
+    stockAvailable : Boolean
+}, {timestamps : true}) //users
 
 //Validation:
 //require:true
